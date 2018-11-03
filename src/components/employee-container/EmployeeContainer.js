b94129091e1621f5e06fefb8d3066f368ed99172
Albrik Dürsteler <albrik.duersteler@moobyte.ch>
2018-11-03 12:58:57 +0100

    added EmployeeContainer

---
+++ src/components/employee-container/EmployeeContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './EmployeeContainer.css';
+
+class EmployeeContainer extends Component {
+    render() {
+        return (
+            <div className="EmployeeContainer">
+                <h1>Mitarbeiter</h1>
+                <div className="EmployeeTable">
+                    Lorem
+                </div>
+                <input type="button" value="+ Hinzufügen"/>
+            </div>
+        );
+    }
+}
+
+export default EmployeeContainer;

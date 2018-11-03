3998c0ed62f274165dfa45850553552c61c31978
christianpetri <christianpetri@hotmail.com>
2018-11-03 12:57:14 +0100

    Added RolesContainer and RolesTable

---
+++ src/components/roles-conatiner/RolesContainer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './RolesContainer.css';
+
+class RolesContainer extends Component {
+    render() {
+
+		const element = (
+			<div>
+				<h1>Roles</h1>
+				<h1>Rollen Tabelle</h1>
+				<button>Rolle hinzufügen</button>
+			</div>
+		);
+
+
+        return (
+			element
+    );
+    }
+}
+
+export default RolesContainer;
